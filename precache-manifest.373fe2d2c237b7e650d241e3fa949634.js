73123f9816ae94382a5e26374b566dc1f1454bbc
Miguel Araujo <miguelant.araujo@gmail.com>
2020-05-01 18:15:17 -0400

    Updates

---
+++ precache-manifest.373fe2d2c237b7e650d241e3fa949634.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "412dc155bcf026d961924a8bd2a30e6a",
+    "url": "/unapec-pensum-tracker/index.html"
+  },
+  {
+    "revision": "bd9c5015f726a0b01348",
+    "url": "/unapec-pensum-tracker/static/css/main.be8ae811.chunk.css"
+  },
+  {
+    "revision": "72fbbd19e73458a84c90",
+    "url": "/unapec-pensum-tracker/static/js/2.135a4c5c.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/unapec-pensum-tracker/static/js/2.135a4c5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd9c5015f726a0b01348",
+    "url": "/unapec-pensum-tracker/static/js/main.8b44e269.chunk.js"
+  },
+  {
+    "revision": "f4aba210f1a4d638b51c",
+    "url": "/unapec-pensum-tracker/static/js/runtime-main.e3a183aa.js"
+  }
+]);
