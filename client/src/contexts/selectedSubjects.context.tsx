c788bac697ed1cbceaaa67431c5394a7dccee9e2
Miguel Araujo <miguelant.araujo@gmail.com>
2020-07-26 09:36:45 -0400

    Added another action to SelectedSubjects context

--- client/src/contexts/selectedSubjects.context.tsx
+++ client/src/contexts/selectedSubjects.context.tsx
@@ -5,18 +5,21 @@ import pensumsJson from "../pensums.json";
 import { Subject } from "../interfaces/pensums.interface";
 
 type SelectedSubjectsAction = {
-    type: "select-subject";
+    type: "select-subject" | "bulk-select";
     payload: {
         pensumCode: string;
         subject: Subject;
+        newSelectedSubjects: Subject[];
+        periodSubjectsCount: number;
+        checkboxStatus: "unchecked" | "indeterminate" | "checked";
     };
 };
 
 type SelectedSubjectsDispatch = (action: SelectedSubjectsAction) => void;
 
-const SelectedSubjectsStateContext = React.createContext<SelectedSubjects | undefined>(
-    undefined
-);
+const SelectedSubjectsStateContext = React.createContext<
+    SelectedSubjects | undefined
+>(undefined);
 
 const SelectedSubjectsDispatchContext = React.createContext<
     SelectedSubjectsDispatch | undefined
@@ -26,14 +29,20 @@ function selectedSubjectsReducer(
     state: SelectedSubjects,
     action: SelectedSubjectsAction
 ) {
-    const { pensumCode, subject } = action.payload;
+    const {
+        pensumCode,
+        subject,
+        newSelectedSubjects,
+        periodSubjectsCount,
+        checkboxStatus,
+    } = action.payload;
 
     switch (action.type) {
         case "select-subject": {
             // Temporary variable to hold changes
             let temp: Subject[];
             const subjects = state[pensumCode];
-            let subjectsToRemove: string[] = []
+            let subjectsToRemove: string[] = [];
 
             // If subject has already been selected, remove it
             if (
@@ -65,8 +74,74 @@ function selectedSubjectsReducer(
 
             return {
                 ...state,
-                [pensumCode]: temp
+                [pensumCode]: temp,
+            };
+        }
+        case "bulk-select": {
+            const notInSelectedSubjects: Subject[] = [];
+            const subjects = state[pensumCode];
+            let temp: Subject[] = [];
+
+            if (checkboxStatus === "unchecked") {
+                temp = subjects.concat(...newSelectedSubjects);
+            } else if (checkboxStatus === "indeterminate") {
+                if (newSelectedSubjects.length < periodSubjectsCount) {
+                    let subjectsToRemove = newSelectedSubjects.map(
+                        (newSelectedSubject) => newSelectedSubject.code
+                    );
+
+                    for (const subjectToRemove of subjectsToRemove) {
+                        subjectsToRemove = getAllPrerequisiteSubjects(
+                            subjects,
+                            subjectToRemove,
+                            subjectsToRemove
+                        );
+                    }
+
+                    subjectsToRemove = [...new Set(subjectsToRemove)];
+
+                    temp = subjects.filter(
+                        (subject: Subject) =>
+                            !subjectsToRemove.includes(subject.code)
+                    );
+                } else {
+                    newSelectedSubjects.forEach((newSelectedSubject) => {
+                        const isInSelectedSubjects = subjects.some(
+                            (subject: Subject) =>
+                                subject.code === newSelectedSubject.code
+                        );
+
+                        if (!isInSelectedSubjects)
+                            notInSelectedSubjects.push(newSelectedSubject);
+                    });
+
+                    temp = subjects.concat(...notInSelectedSubjects);
+                }
+            } else if (checkboxStatus === "checked") {
+                let subjectsToRemove = newSelectedSubjects.map(
+                    (nss) => nss.code
+                );
+
+                for (const subjectToRemove of subjectsToRemove) {
+                    subjectsToRemove = getAllPrerequisiteSubjects(
+                        subjects,
+                        subjectToRemove,
+                        subjectsToRemove
+                    );
+                }
+
+                subjectsToRemove = [...new Set(subjectsToRemove)];
+
+                temp = subjects.filter(
+                    (subject: Subject) =>
+                        !subjectsToRemove.includes(subject.code)
+                );
             }
+            
+            return {
+                ...state,
+                [pensumCode]: temp,
+            };
         }
         default: {
             throw new Error(`Unhandled action type: ${action.type}`);
@@ -86,7 +161,10 @@ if (Object.keys(defaultSelectedSubjects).length === 0) {
 }
 
 function SelectedSubjectsProvider({ children }: { children: React.ReactNode }) {
-    const [state, dispatch] = React.useReducer(selectedSubjectsReducer, defaultSelectedSubjects);
+    const [state, dispatch] = React.useReducer(
+        selectedSubjectsReducer,
+        defaultSelectedSubjects
+    );
 
     return (
         <SelectedSubjectsStateContext.Provider value={state}>
