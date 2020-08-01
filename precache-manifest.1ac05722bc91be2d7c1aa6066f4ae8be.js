6c3e82b6ad3a4411d177cf80ca52a3b8364a4d4e
Miguel Araujo <miguelant.araujo@gmail.com>
2020-07-31 20:01:35 -0400

    Updates

---
+++ precache-manifest.1ac05722bc91be2d7c1aa6066f4ae8be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1329fd9b218da2d8a7572827cd9b023",
+    "url": "/unapec-pensum-tracker/index.html"
+  },
+  {
+    "revision": "0bdabe190bce5ef760d8",
+    "url": "/unapec-pensum-tracker/static/css/main.7a1f1e0c.chunk.css"
+  },
+  {
+    "revision": "1c7858b022a611360d74",
+    "url": "/unapec-pensum-tracker/static/js/2.f26e04a7.chunk.js"
+  },
+  {
+    "revision": "eac4cf64bf573541fd6dba29c78393a0",
+    "url": "/unapec-pensum-tracker/static/js/2.f26e04a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0bdabe190bce5ef760d8",
+    "url": "/unapec-pensum-tracker/static/js/main.012b2c6c.chunk.js"
+  },
+  {
+    "revision": "f4aba210f1a4d638b51c",
+    "url": "/unapec-pensum-tracker/static/js/runtime-main.e3a183aa.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/unapec-pensum-tracker/precache-manifest.793a2d771cd2aed50b688e86facf7374.js"
+  "/unapec-pensum-tracker/precache-manifest.1ac05722bc91be2d7c1aa6066f4ae8be.js"
 );
 
 self.addEventListener('message', (event) => {
