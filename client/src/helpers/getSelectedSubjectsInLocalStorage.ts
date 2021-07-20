330f0209e30fe6ab1ad7fbe5e0ae0037916be131
Miguel Araujo <miguelant.araujo@gmail.com>
2021-07-19 23:56:49 -0400

    Update dependencies to latest versions (client/scraper)
    Also misc changes to fix deprecations in new versions

--- client/src/helpers/getSelectedSubjectsInLocalStorage.ts
+++ client/src/helpers/getSelectedSubjectsInLocalStorage.ts
@@ -1,17 +1,19 @@
 import { SelectedSubjects } from "../interfaces/selectedSubjects.interface";
 import { Pensum } from "../interfaces/pensums.interface";
 
-export default function (pensumsJson?: Pensum[]) {
-    const savedSelectedSubjects = localStorage.getItem("selectedSubjects");
-    const defaultSelectedSubjects: SelectedSubjects = savedSelectedSubjects
-        ? JSON.parse(savedSelectedSubjects)
-        : {};
+export default function getSelectedSubjectsInLocalStorage(
+  pensumsJson?: Pensum[]
+) {
+  const savedSelectedSubjects = localStorage.getItem("selectedSubjects");
+  const defaultSelectedSubjects: SelectedSubjects = savedSelectedSubjects
+    ? JSON.parse(savedSelectedSubjects)
+    : {};
 
-    if (pensumsJson && Object.keys(defaultSelectedSubjects).length === 0) {
-        for (const pensum of pensumsJson) {
-            defaultSelectedSubjects[pensum.pensumCode] = [];
-        }
+  if (pensumsJson && Object.keys(defaultSelectedSubjects).length === 0) {
+    for (const pensum of pensumsJson) {
+      defaultSelectedSubjects[pensum.pensumCode] = [];
     }
+  }
 
-    return defaultSelectedSubjects;
+  return defaultSelectedSubjects;
 }
