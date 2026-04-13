a6afb821c5eaf1a43cc298f2466c0f4ff486df93
Miguel Araujo <38594993+Maikuh@users.noreply.github.com>
2026-04-12 21:43:42 -0400

    Fix next config for gh pages

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  reactCompiler: true,
-};
+	/* config options here */
+	output: 'export',
+	basePath: '/unapec-pensum-tracker',
+	reactCompiler: true,
+	images: {
+		unoptimized: true,
+	},
+}
 
-export default nextConfig;
+export default nextConfig
