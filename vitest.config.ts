c6984f771a284c0e5130a6dd46c3eac678210dc4
Miguel Araujo <38594993+Maikuh@users.noreply.github.com>
2026-04-16 23:04:25 -0400

    Add test coverage

--- vitest.config.ts
+++ vitest.config.ts
@@ -7,6 +7,9 @@ export default defineConfig({
 		environment: 'jsdom',
 		include: ['**/*.test.ts', '**/*.test.tsx'],
 		globals: true,
+		coverage: {
+			provider: 'istanbul',
+		},
 	},
 	resolve: {
 		tsconfigPaths: true,
