1e15f9b6b772c37829fa6bc1448069df92c32d6c
Maikuh <Maikuh@users.noreply.github.com>
2026-04-18 15:26:54 +0000

    deploy: 256d7e6488ec8d7dff433bfdd6cc0054154d9190

---
+++ sw.js
@@ -0,0 +1,41 @@
+const CACHE = 'pensum-v1'
+
+self.addEventListener('install', (e) => {
+	e.waitUntil(caches.open(CACHE).then((c) => c.add(self.registration.scope)))
+	self.skipWaiting()
+})
+
+self.addEventListener('activate', (e) => {
+	e.waitUntil(
+		caches
+			.keys()
+			.then((keys) =>
+				Promise.all(
+					keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
+				),
+			),
+	)
+	self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+	const { request } = e
+	const url = new URL(request.url)
+
+	if (url.pathname.includes('/_next/static/')) {
+		e.respondWith(
+			caches.match(request).then(
+				(cached) =>
+					cached ??
+					fetch(request).then((res) => {
+						caches.open(CACHE).then((c) => c.put(request, res.clone()))
+						return res
+					}),
+			),
+		)
+	} else if (request.mode === 'navigate') {
+		e.respondWith(
+			fetch(request).catch(() => caches.match(self.registration.scope)),
+		)
+	}
+})
