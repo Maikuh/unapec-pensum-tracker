4b19dc7c9ada5f0808c57e82fe4f0b2d6b0096d7
Miguel Araujo <38594993+Maikuh@users.noreply.github.com>
2026-04-18 11:17:50 -0400

    Remove unused cache config

--- next.config.ts
+++ next.config.ts
@@ -3,26 +3,11 @@ import type { NextConfig } from 'next'
 const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? '/unapec-pensum-tracker'
 
 const nextConfig: NextConfig = {
-	/* config options here */
 	output: 'export',
 	basePath,
 	reactCompiler: true,
 	images: {
 		unoptimized: true,
-	},
-	async headers() {
-		return [
-			{
-				source: '/_next/static/(.*)',
-				headers: [
-					{
-						key: 'Cache-Control',
-						// Cache for 1 year (31536000 seconds)
-						value: 'public, max-age=31536000, immutable',
-					},
-				],
-			},
-		]
 	},
 }
 
