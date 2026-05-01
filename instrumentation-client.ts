64dedb6ba95370cabad443e6141ec71e3da510b3
Miguel Araujo <38594993+Maikuh@users.noreply.github.com>
2026-04-30 21:08:50 -0400

    chore: add zed config, format, disable posthog debug and set cursor pointer on buttons

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -5,5 +5,4 @@ posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN ?? '', {
 	ui_host: 'https://us.posthog.com',
 	defaults: '2026-01-30',
 	capture_exceptions: true,
-	debug: process.env.NODE_ENV === 'development',
 })
